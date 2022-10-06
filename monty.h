4b458226326a2356ee564b1a1c4e0fc70bff7541
Wendo324 <githakawendowanjiru@gmail.com>
2022-10-06 14:46:00 -0700

    t1

---
+++ monty.h
@@ -0,0 +1,77 @@
+#ifndef MONTY
+#define MONTY
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+  int n;
+  struct stack_s *prev;
+  struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+  char *opcode;
+  void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct var_global - structure with the first addres of elements
+ * @first: Pointer to stack
+ * @buffer: The buffer
+ * @file: The file descriptor of fopen.
+ */
+typedef struct var_global
+{
+  stack_t **first;
+  char *buffer;
+  FILE *file;
+} global_t;
+extern global_t header;
+
+/* Main functions */
+void start_stack(stack_t **stack);
+void function_select(stack_t **stack, unsigned int line_number, char *command);
+void find_file(char *path, stack_t **stack);
+int manage_error(char *value, unsigned int line_number);
+
+/* Command functions */
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
+void _mod(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void _rotl(stack_t **stack, unsigned int line_number);
+void _rotr(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+
+void free_all(void);
+
+#endif
